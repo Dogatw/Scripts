cf03b43c18dbc1424668aaece6c0a87a99f44b33
Dogatw <63521104+Dogatw@users.noreply.github.com>
2021-07-15 19:46:49 +0530

    Update and rename Village Renamer.user.js to Cren.js

---
+++ Cren.js
@@ -0,0 +1,19 @@
+var popPackets = 1000;
+var heavyPop = 4;
+var troopsName = {
+    archer: "arc",
+    axe: "axe",
+    catapult: "cat",
+    heavy: "HC",
+    light: "LC",
+    marcher: "MC",
+    ram: "ram",
+    snob: "noble",
+    spy: "spy",
+    sword: "sw",
+    spear: "sp",
+    knight: "pal",
+    packets: "packets"
+};
+$.getScript('https://dl.dropboxusercontent.com/s/943wux6kq026hft/commandRenamerInterface.js?dl=0');
+void(0);
