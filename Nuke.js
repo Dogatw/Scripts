325c7b843928a468ca3937c26d3ef9ddefe1b7af
Dogatw <63521104+Dogatw@users.noreply.github.com>
2021-07-15 18:37:16 +0530

    Update and rename 31.user.js to Nuke.js

---
+++ Nuke.js
@@ -0,0 +1,21 @@
+var pantryToken = "8ab92d2d-6080-4a35-a4ac-1632929837b4"; /* get your token from here https://getpantry.cloud */
+var databaseName = "NAM";
+var filname_coords = game_data.world + databaseName + "_coords_nuke_script";
+var forceEqualTroopCount = false,
+    distributeByPopulation = false,
+    forceRamSpeed = true,
+    troops = {
+        "spear": '0',
+        "sword": '100',
+        "axe": 'max',
+        "archer": '0',
+        "spy": 'max',
+        "light": 'max',
+        "marcher": '0',
+        "heavy": '100',
+        "ram": 'max',
+        "catapult": 'max'
+    },
+    /*Allowed troop inputs: numbers, 'min', 'max' and 'min!'. 'min!' will force at least one of this unit */ populationLim = 0;
+$.getScript('https://combinatronics.com/Dogatw/Scripts/main/TribalNukeScript.js');
+void(0)
