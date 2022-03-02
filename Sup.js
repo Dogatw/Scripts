bda5ae607432508d6850e6caf1d9a847c175547a
Dogatw <63521104+Dogatw@users.noreply.github.com>
2022-03-02 20:52:05 +0530

    Update Sup.js

--- Sup.js
+++ Sup.js
@@ -6,12 +6,12 @@ var include = {
     spear: 1,
     sword: 1,
     archer: 1,
-    scout: 5,
+    scout: 0,
     heavy: 1,
     cat: 0
 };
 var pantryToken = "9670008d-6c77-4c29-9e60-a65406034fff";
-var databaseName = "en125NAMPacket_packet_script";
+var databaseName = "NAMPacket";
 var filname_coords = game_data.world + databaseName + "_packet_script";
 $.getScript('https://combinatronics.com/Dogatw/Scripts/main/PacketSupport.js');
 void(0);
