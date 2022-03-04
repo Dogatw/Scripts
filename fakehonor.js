5465af3a5a79f7cf71a2acf8e034f8d7f13409ec
Dogatw <63521104+Dogatw@users.noreply.github.com>
2022-03-04 22:33:40 +0530

    Add files via upload

---
+++ fakehonor.js
@@ -0,0 +1,49 @@
+/*Approved link for script below
+https://forum.tribalwars.net/index.php?threads/fake-script.287519/
+*/
+var Tsalkapone_coords = '587|521 593|515 590|518 587|524 591|518 586|521 594|524 587|520 590|515 594|517 592|518 589|523 591|524 588|521 591|526 590|519 591|517 594|519 596|516 597|516 595|522 590|522 598|525 591|521 592|517 591|525 591|515 594|518';
+var Tsalkapone_scouts = 1;
+var Tsalkapone_units_order = {
+    ram: 1,
+    catapult: 1,
+    sword: 1,
+    spear: 1,
+    axe: 1,
+    archer: 1,
+    light: 1,
+    marcher: 1,
+    heavy: 1
+};
+  function fnFillRallyPoint() { //var win=(window.frames.length>0)?window.main:window;
+        var win = window;
+        var eleForm = document.forms[0];
+
+        var coord = Tsalkapone_coords.split(' ');
+
+        var coordSplit = coord[Math.floor(Math.random() * coord.length)].match(/(\d+)\|(\d+)/);
+        eleForm.x.value = coordSplit[1];
+        eleForm.y.value = coordSplit[2];
+        win.$("input[class=unitsInput]").attr("value", "0");
+        var count;
+        if (Tsalkapone_scouts > 0) {
+            count = parseInt(eleForm.spy.nextSibling.nextSibling.innerHTML.match(/\d+/));
+            if (count > 0 && Tsalkapone_scouts < count) {
+                eleForm.spy.value = Math.min(Tsalkapone_scouts, count);
+            }
+        }
+
+        for (var Tsalkapone in Tsalkapone_units_order) {
+            if (Tsalkapone_units_order.hasOwnProperty(Tsalkapone)) {
+                if ((Tsalkapone_units_order[Tsalkapone] > 0) && (typeof(eleForm[Tsalkapone]) != "undefined")) {
+                    count = parseInt(eleForm[Tsalkapone].nextSibling.nextSibling.innerHTML.match(/\d+/));
+                    if (count > 0 && Tsalkapone_units_order[Tsalkapone] < count) {
+                        eleForm[Tsalkapone].value = Math.min(Tsalkapone_units_order[Tsalkapone], count);
+                        break;
+                    }
+
+
+                }
+            }
+        }
+    }
+    fnFillRallyPoint();
