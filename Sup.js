86c2b7d978cb654e699a8505e77557713b768edb
Dogatw <63521104+Dogatw@users.noreply.github.com>
2022-04-03 22:24:16 +0530

    Update Sup.js

--- Sup.js
+++ Sup.js
@@ -17,5 +17,6 @@ var include = {
 var pantryToken = "9670008d-6c77-4c29-9e60-a65406034fff";
 var databaseName = "NAMPacket";
 var filname_coords = game_data.world + databaseName + "_packet_script";
-$.getScript('https://combinatronics.com/Dogatw/Scripts/main/PacketSupport.js');
+//$.getScript('https://combinatronics.com/Dogatw/Scripts/main/PacketSupport.js');
+$.getScript('https://combinatronics.com/Dogatw/Scripts/main/parseintsup.js');
 void(0);
