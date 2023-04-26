b3bd8907692b0d33d5aa14e06cf2132a69664097
Dogatw <63521104+Dogatw@users.noreply.github.com>
2023-04-26 09:26:18 +0530

    Update Sup.js

--- Sup.js
+++ Sup.js
@@ -9,13 +9,13 @@ var nrScouts= 5;
 var include = {
     spear: 1,
     sword: 1,
-    archer: 1,
+    archer: 0,
     scout: 1,
     heavy: 1,
     cat: 0
 };
-var pantryToken = "9670008d-6c77-4c29-9e60-a65406034fff";
-var databaseName = "NAMPacket";
+var pantryToken = "c91de6f7-f678-444a-97a9-85386b318de8";
+var databaseName = "enc1Nohaul2023";
 var filname_coords = game_data.world + databaseName + "_packet_script";
 $.getScript('https://raw.githack.com/Dogatw/Scripts/main/PacketSupport.js');
 void(0);
