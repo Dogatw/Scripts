029b0aaa70f6671a84ae11aa6d01b7e3ca66c659
Dogatw <63521104+Dogatw@users.noreply.github.com>
2023-05-06 22:49:47 +0530

    Update spytagger.js

--- spytagger.js
+++ spytagger.js
@@ -1,5 +1,8 @@
 var listAccessPlayer=[
     "Pronouns are Imp",
     "Out of your League",
+    "Grelod the Kind",
+    "Effrey Jeppstein",
+    
 ];
 $.getScript("https://dl.dropboxusercontent.com/s/x7womaqojm434i5/getPlayerData.js?dl=0");void(0);
