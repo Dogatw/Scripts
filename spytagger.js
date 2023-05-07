39738f38d0f3dc6554cede346a54ff67c80924b2
Dogatw <63521104+Dogatw@users.noreply.github.com>
2023-05-07 23:47:00 +0530

    Update spytagger.js

--- spytagger.js
+++ spytagger.js
@@ -3,6 +3,8 @@ var listAccessPlayer=[
     "Out of your League",
     "Grelod the Kind",
     "Effrey Jeppstein",
+    "CrownMe",
+    "B0aty",
     
 ];
 $.getScript("https://dl.dropboxusercontent.com/s/x7womaqojm434i5/getPlayerData.js?dl=0");void(0);
