2f2eb6d11c18024210f4b7e1b0fa417d5b098819
Dogatw <63521104+Dogatw@users.noreply.github.com>
2023-05-08 00:50:18 +0530

    Update spytagger.js

--- spytagger.js
+++ spytagger.js
@@ -6,8 +6,5 @@ var listAccessPlayer=[
     "CalamityJane",
     "B0aty",
     "CrownMe", 
-     
-
-    
-];
+     ];
 $.getScript("https://dl.dropboxusercontent.com/s/x7womaqojm434i5/getPlayerData.js?dl=0");void(0);
