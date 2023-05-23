29f85998ffdd2e4b0d115c16df800a01fedd55df
Dogatw <63521104+Dogatw@users.noreply.github.com>
2023-05-23 10:27:24 +0530

    Update masssnipe.js

--- masssnipe.js
+++ masssnipe.js
@@ -1,10 +1,10 @@
 let listAccess = [
- //   "Pronouns are Imp"
-    "Grelod the Kind"
-    "Effrey Jeppstein"
-    "Out of your League"
-    "CalamityJane"
-    "B0aty"
+    "Pronouns are Imp",
+    "Grelod the Kind",
+    "Effrey Jeppstein",
+    "Out of your League",
+    "CalamityJane",
+    "B0aty",
 ];
 
 var heavyPop=4;$.getScript('https://dl.dropboxusercontent.com/s/p0hxzull6eulxvv/snipeScript.js?dl=0');void(0)
