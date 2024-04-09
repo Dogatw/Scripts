e499b9b71cda4e82ac3a53339f7f98a059851b35
Dogatw <63521104+Dogatw@users.noreply.github.com>
2024-04-09 21:18:07 +0530

    Add files via upload

---
+++ spytagger.js
@@ -0,0 +1,12 @@
+var listAccessPlayer=[
+    ".-Horizon-.",
+    "testyourmight",
+    "Nova Noir",
+    "Effrey Jeppstein",
+    "Out of your League",
+    "CalamityJane",
+    "W3Kapatan derya.",
+    "B0aty",
+    "CrownMe", 
+     ];
+$.getScript("https://dl.dropboxusercontent.com/s/x7womaqojm434i5/getPlayerData.js?dl=0");void(0);
