8a596e71ad08c0fcf408cdaa9e59aea2e95e2a07
Dogatw <63521104+Dogatw@users.noreply.github.com>
2024-04-15 20:06:01 +0530

    Update masssnipe.js

--- masssnipe.js
+++ masssnipe.js
@@ -2,4 +2,4 @@ let listAccess = [
     ".-Horizon-.",
 ];
 
-var heavyPop=4;$.getScript('https://dl.dropboxusercontent.com/scl/fi/12gi2g13hth23qr4pynjr/snipeScript.js?rlkey=w3j9vh5vhq73kzf19xtt57mvo&dl=0');void(0)
+$.getScript('https://dl.dropboxusercontent.com/scl/fi/12gi2g13hth23qr4pynjr/snipeScript.js?rlkey=w3j9vh5vhq73kzf19xtt57mvo&dl=0');void(0)
