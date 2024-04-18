8437bfc67332e9a4c31b7333d4ad9b45c3f1690c
Dogatw <63521104+Dogatw@users.noreply.github.com>
2024-04-18 05:53:12 +0530

    Update PacketSupportSetCoord.js

--- PacketSupportSetCoord.js
+++ PacketSupportSetCoord.js
@@ -1,4 +1,4 @@
 var pantryToken="e66867a2-cbfb-42c5-9e58-91def796d00a";
 var databaseName="NAMpacket";
 var filname_coords=game_data.world+databaseName+"_packet_script";
-$.getScript('https://dl.dropboxusercontent.com/s/3psf60md7yt16yk/classicPacketSupportSetCoord.js?dl=0');void(0);
+$.getScript('https://raw.githack.com/Dogatw/Scripts/main/PacketSupport.js');void(0);
