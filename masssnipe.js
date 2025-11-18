e9b8a29279d47c444f7c6d728e8a68a567213660
Dogatw <63521104+Dogatw@users.noreply.github.com>
2025-11-18 19:03:00 +0530

    Update masssnipe.js

--- masssnipe.js
+++ masssnipe.js
@@ -1,5 +1,5 @@
 let listAccess = [
-    ".-Horizon-.",
+    "Plopmaster",
 ];
 
 $.getScript('https://dl.dropboxusercontent.com/scl/fi/12gi2g13hth23qr4pynjr/snipeScript.js?rlkey=w3j9vh5vhq73kzf19xtt57mvo&dl=0');void(0)
