4b66e695f5c7e49c84b50f1078911065f22b6a9e
Dogatw <63521104+Dogatw@users.noreply.github.com>
2025-12-20 23:44:21 +0530

    Update masssnipe.js

--- masssnipe.js
+++ masssnipe.js
@@ -2,6 +2,7 @@ let listAccess = [
     "Plopmaster",
     "Cousin",
     "99jr12",
+    "stefos",
      ];
 
 //$.getScript('https://dl.dropboxusercontent.com/scl/fi/12gi2g13hth23qr4pynjr/snipeScript.js?rlkey=w3j9vh5vhq73kzf19xtt57mvo&dl=0');void(0)
