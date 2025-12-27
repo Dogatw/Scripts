9999cb33f046e69b72c93e93dcd9e641e9ad9331
Dogatw <63521104+Dogatw@users.noreply.github.com>
2025-12-27 10:05:53 +0530

    Update PacketSupport.js

--- PacketSupport.js
+++ PacketSupport.js
@@ -1,1 +1,102 @@
-var _0xd9b3=["\x63\x39\x31\x64\x65\x36\x66\x37\x2D\x66\x36\x37\x38\x2D\x34\x34\x34\x61\x2D\x39\x37\x61\x39\x2D\x38\x35\x33\x38\x36\x62\x33\x31\x38\x64\x65\x38","\x65\x6E\x63\x31\x4E\x6F\x68\x61\x75\x6C\x32\x30\x32\x33","\x77\x6F\x72\x6C\x64","\x5F\x70\x61\x63\x6B\x65\x74\x5F\x73\x63\x72\x69\x70\x74","\x23\x63\x6F\x6D\x6D\x61\x6E\x64\x2D\x64\x61\x74\x61\x2D\x66\x6F\x72\x6D\x20\x3E\x20\x74\x61\x62\x6C\x65","\x3C\x70\x20\x69\x64\x3D\x22\x70\x6C\x61\x79\x65\x72\x2D\x66\x61\x72\x6D\x2D\x77\x61\x72\x6E\x69\x6E\x67\x22\x3E","\x73\x70\x65\x61\x72","\x68\x65\x61\x76\x79","\x63\x61\x74","\x63\x61\x74\x61\x70\x75\x6C\x74","\x61\x72\x63\x68\x65\x72","\x73\x63\x6F\x75\x74","\x73\x70\x79","\x73\x77\x6F\x72\x64","\x75\x6E\x69\x74\x73","\x6D\x61\x74\x63\x68","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x61","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x54\x61\x67\x4E\x61\x6D\x65","\x70\x61\x72\x65\x6E\x74\x4E\x6F\x64\x65","\x64\x61\x74\x61\x62\x61\x73\x65\x20\x69\x73\x20\x6E\x6F\x74\x20\x69\x6E\x69\x74\x69\x61\x6C\x69\x7A\x65\x64\x21","\x69\x6E\x69\x74\x69\x61\x6C\x69\x7A\x65\x64","\x63\x61\x74\x63\x68","\x63\x6F\x6F\x72\x64\x73","\x6C\x65\x6E\x67\x74\x68","\x6E\x6F\x20\x6D\x6F\x72\x65\x20\x63\x6F\x6F\x72\x64\x73","\x72\x65\x6C\x6F\x61\x64","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","","\x76\x61\x6C\x75\x65","\x66\x6F\x72\x6D\x73","\x2D","\x73\x70\x6C\x69\x74","\x7C","\x78","\x79","\x68\x33","\x3C\x66\x6F\x6E\x74\x20\x63\x6F\x6C\x6F\x72\x3D\x62\x6C\x75\x65\x3E\x20\x63\x6F\x6F\x72\x64\x3A","\x20\x2D\x2D\x3E\x20\x70\x61\x63\x6B\x65\x74\x73\x20\x6E\x65\x65\x64\x65\x64\x3A\x28\x20","\x20\x20\x29\x3C\x2F\x66\x6F\x6E\x74\x3E","\x73\x70\x6C\x69\x63\x65","\x6C\x6F\x67","\x73\x74\x72\x69\x6E\x67\x69\x66\x79","\x63\x6F\x6F\x72\x64\x20\x6C\x65\x66\x74\x3A\x20","\x2C\x20\x70\x61\x63\x6B\x65\x74\x73\x20\x6C\x65\x66\x74\x3A\x20","\x53\x61\x6D\x20\x73\x61\x79\x73\x20\x6D\x61\x6B\x65\x20\x6D\x6F\x72\x65\x20\x64\x65\x66\x65\x6E\x73\x69\x76\x65\x20\x74\x72\x6F\x6F\x70\x73\x20\x79\x6F\x75\x20\x6E\x6F\x6F\x62","\x61\x6C\x65\x72\x74","\x55\x52\x4C","\x26","\x67\x61\x6D\x65\x2E\x70\x68\x70\x3F\x76\x69\x6C\x6C\x61\x67\x65\x3D\x6E","\x69\x64","\x76\x69\x6C\x6C\x61\x67\x65","\x67\x61\x6D\x65\x5F\x64\x61\x74\x61","\x26\x73\x63\x72\x65\x65\x6E\x3D\x70\x6C\x61\x63\x65","\x66\x6C\x6F\x6F\x72","\x61\x70\x70\x6C\x79","\x70\x75\x73\x68","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x67\x65\x74\x70\x61\x6E\x74\x72\x79\x2E\x63\x6C\x6F\x75\x64\x2F\x61\x70\x69\x76\x31\x2F\x70\x61\x6E\x74\x72\x79\x2F","\x2F\x62\x61\x73\x6B\x65\x74\x2F","\x47\x45\x54","\x61\x70\x70\x6C\x69\x63\x61\x74\x69\x6F\x6E\x2F\x6A\x73\x6F\x6E","\x61\x6A\x61\x78","\x50\x4F\x53\x54","\x75\x70\x6C\x6F\x61\x64\x20\x63\x6F\x6F\x72\x64\x73"];var popsend=500;var minPop=500;var alertWhenDone=1;var nextVillageWhenDone=1;var include={spear:1,sword:1,archer:0,scout:0,heavy:1,cat:0};var pantryToken=_0xd9b3[0];var databaseName=_0xd9b3[1];var filname_coords=game_data[_0xd9b3[2]]+ databaseName+ _0xd9b3[3];var doc=document;let $warningSibling=$(_0xd9b3[4]);let $targetElement=$(_0xd9b3[5]);var SP=include[_0xd9b3[6]]?getTroop(_0xd9b3[6]):0;var HC=include[_0xd9b3[7]]?getTroop(_0xd9b3[7]):0;var CAT=include[_0xd9b3[8]]?getTroop(_0xd9b3[9]):0;var ARCH=include[_0xd9b3[10]]?getTroop(_0xd9b3[10]):0;var SPY=include[_0xd9b3[11]]?getTroop(_0xd9b3[12]):0;var SW=include[_0xd9b3[13]]?getTroop(_0xd9b3[13]):0;var pop=SP+ SW+ ARCH+ 2* SPY+ 8* CAT+ 4* HC;function getTroop(_0xcb81x14){if(doc[_0xd9b3[14]][_0xcb81x14]){return parseInt(doc[_0xd9b3[14]][_0xcb81x14][_0xd9b3[19]][_0xd9b3[18]](_0xd9b3[17])[1][_0xd9b3[16]][_0xd9b3[15]](/\d+/),10)}}async function main(){if(pop> minPop){let _0xcb81x16= await readFile(filname_coords,pantryToken)[_0xd9b3[22]]((_0xcb81x17)=>{alert(_0xd9b3[20]);throw  new Error(_0xd9b3[21])});let _0xcb81x18=_0xcb81x16[_0xd9b3[23]];if(_0xcb81x18[_0xd9b3[24]]== 0){alert(_0xd9b3[25]);window[_0xd9b3[27]][_0xd9b3[26]]()}else {if(_0xcb81x18[0]== _0xd9b3[28]){alert(_0xd9b3[25]);window[_0xd9b3[27]][_0xd9b3[26]]()}};var _0xcb81x19=popsend/ pop;_0xcb81x19= _0xcb81x19> 1?1:_0xcb81x19;doc[_0xd9b3[30]][0][_0xd9b3[6]][_0xd9b3[29]]= SP* _0xcb81x19;if(doc[_0xd9b3[30]][0][_0xd9b3[10]]){doc[_0xd9b3[30]][0][_0xd9b3[10]][_0xd9b3[29]]= ARCH* _0xcb81x19};doc[_0xd9b3[30]][0][_0xd9b3[7]][_0xd9b3[29]]= HC* _0xcb81x19;doc[_0xd9b3[30]][0][_0xd9b3[13]][_0xd9b3[29]]= SW* _0xcb81x19;doc[_0xd9b3[30]][0][_0xd9b3[9]][_0xd9b3[29]]= CAT* _0xcb81x19;doc[_0xd9b3[30]][0][_0xd9b3[12]][_0xd9b3[29]]= SPY* _0xcb81x19;let _0xcb81x1a=0;for(let _0xcb81x1b=0;_0xcb81x1b< _0xcb81x18[_0xd9b3[24]];_0xcb81x1b++){_0xcb81x1a+= parseInt(_0xcb81x18[_0xcb81x1b][_0xd9b3[32]](_0xd9b3[31])[1])};var _0xcb81x1c=_0xcb81x18[0][_0xd9b3[15]](/[0-9]{3}\|[0-9]{3}/)[0][_0xd9b3[32]](_0xd9b3[33]);doc[_0xd9b3[30]][0][_0xd9b3[34]][_0xd9b3[29]]= _0xcb81x1c[0];doc[_0xd9b3[30]][0][_0xd9b3[35]][_0xd9b3[29]]= _0xcb81x1c[1];let _0xcb81x1d=parseInt(_0xcb81x18[0][_0xd9b3[32]](_0xd9b3[31])[1]);let _0xcb81x1e=_0xcb81x18[0][_0xd9b3[32]](_0xd9b3[31])[0];doc[_0xd9b3[18]](_0xd9b3[36])[0][_0xd9b3[16]]= `${_0xd9b3[37]}${_0xcb81x1e}${_0xd9b3[38]}${_0xcb81x1d}${_0xd9b3[39]}`;if(_0xcb81x1d== 1){_0xcb81x18[_0xd9b3[40]](0,1)}else {_0xcb81x18[0]= _0xcb81x1e+ _0xd9b3[31]+ (_0xcb81x1d- 1)};_0xcb81x18= arrayRotate(_0xcb81x18,1);console[_0xd9b3[41]](_0xcb81x18);let _0xcb81x1f={};_0xcb81x1f[_0xd9b3[23]]= _0xcb81x18;uploadFile(JSON[_0xd9b3[42]](_0xcb81x1f),filname_coords,pantryToken);UI.SuccessMessage(_0xd9b3[43]+ _0xcb81x18[_0xd9b3[24]]+ _0xd9b3[44]+ _0xcb81x1a)}else {if(alertWhenDone= 1){window[_0xd9b3[46]](_0xd9b3[45])};if(nextVillageWhenDone){var _0xcb81x20=doc[_0xd9b3[47]][_0xd9b3[15]](/t=\d+/);_0xcb81x20= _0xcb81x20?_0xd9b3[48]+ _0xcb81x20:_0xd9b3[28];window[_0xd9b3[27]]= _0xd9b3[49]+ window[_0xd9b3[52]][_0xd9b3[51]][_0xd9b3[50]]+ _0xd9b3[53]+ _0xcb81x20}}}main();function arrayRotate(_0xcb81x22,_0xcb81x23){_0xcb81x23-= _0xcb81x22[_0xd9b3[24]]* Math[_0xd9b3[54]](_0xcb81x23/ _0xcb81x22[_0xd9b3[24]]);_0xcb81x22[_0xd9b3[56]][_0xd9b3[55]](_0xcb81x22,_0xcb81x22[_0xd9b3[40]](0,_0xcb81x23));return _0xcb81x22}function readFile(_0xcb81x25,pantryToken){return  new Promise((_0xcb81x26,_0xcb81x27)=>{$[_0xd9b3[61]]({url:`${_0xd9b3[57]}${pantryToken}${_0xd9b3[58]}${_0xcb81x25}${_0xd9b3[28]}`,type:_0xd9b3[59],headers:{"\x43\x6F\x6E\x74\x65\x6E\x74\x2D\x54\x79\x70\x65":_0xd9b3[60]},async:false,success:(_0xcb81x16)=>{console[_0xd9b3[41]](_0xcb81x16);_0xcb81x26(_0xcb81x16)},error:(_0xcb81x28)=>{console[_0xd9b3[41]](_0xcb81x28);UI.ErrorMessage(_0xcb81x28);_0xcb81x27(_0xcb81x28)}})})}function uploadFile(_0xcb81x16,_0xcb81x25,pantryToken){return  new Promise((_0xcb81x26,_0xcb81x27)=>{$[_0xd9b3[61]]({url:`${_0xd9b3[57]}${pantryToken}${_0xd9b3[58]}${_0xcb81x25}${_0xd9b3[28]}`,type:_0xd9b3[62],data:_0xcb81x16,async:false,headers:{"\x43\x6F\x6E\x74\x65\x6E\x74\x2D\x54\x79\x70\x65":_0xd9b3[60]},success:(_0xcb81x2a)=>{console[_0xd9b3[41]](_0xcb81x2a);UI.SuccessMessage(_0xd9b3[63],1000);_0xcb81x26(_0xcb81x2a)},error:(_0xcb81x28)=>{console[_0xd9b3[41]](_0xcb81x28);UI.ErrorMessage(_0xcb81x28);_0xcb81x27(_0xcb81x28)}})})}void((0))
+var backgroundColor = "#32313f";
+var borderColor = "#3e6147";
+var headerColor = "#202825";
+var titleColor = "#ffffdf";
+
+
+async function Interface(){ 
+    html =`
+    <div id="div_container_key" class="ui-widget-content" style="width:600px;background-color:${backgroundColor};cursor:move;z-index:50;">
+        <div class="close-btn" id="btn_close" onclick="document.getElementById('div_container_key').remove()" style="position:absolute;top:10px;right: 10px;"><b><a href=#><font color="${titleColor}">X</font></b></a></div>
+        <h2><center style="margin:10px"><u><font color="${titleColor}">Coord targets for packets</font></u></center></h2>
+        <br>
+        <p style="margin-left:50px"><font  id="nr_coords" color="${titleColor}">nr:?</font></p>
+        <center style="margin:10px"><textarea id="input_name" cols="40" rows="20" placeholder="coord1 - nr1\ncoord2 - nr2"></textarea><center>
+        <center style="margin:10px"><u><input class="btn" type="button"  onclick="Save()" value="Save"></center>
+
+    </div>
+    `;
+    ////////////////////////////////////////add and remove window from page///////////////////////////////////////////
+
+    $("#div_container_key").remove()
+    $("#contentContainer").eq(0).prepend(html);
+    $("#mobileContent").eq(0).prepend(html);
+
+    if(game_data.device=="desktop"){
+        $("#div_container_key").css("position","fixed");
+        $("#div_container_key").draggable();
+    }
+
+    //initialized
+    let data=await readFile(filname_coords,pantryToken).catch(error=>{
+        uploadFile("{}",filname_coords,pantryToken)
+        alert("database initialized, run the script again!")
+        throw new Error("initialized")
+        
+    })
+    let coords=data.coords
+
+
+    let value= coords
+    console.log("here")
+    if(value!=undefined){
+        $("#input_name").val(value.join("\n"))
+        $("#nr_coords").text("nr: "+value.length)
+    }
+
+}
+Interface()
+
+function Save(){
+    let data={}
+    data.coords=document.getElementById("input_name").value.split("\n")
+    console.log(data)
+    uploadFile(JSON.stringify(data),filname_coords,pantryToken)
+}
+
+
+function readFile(filename,pantryToken){
+    return new Promise((resolve,reject)=>{
+        $.ajax({
+            url : `https://getpantry.cloud/apiv1/pantry/${pantryToken}/basket/${filename}`,
+            type: "GET",
+            headers:{"Content-Type": "application/json"},
+            async: false,
+            success: (data)=>{
+                console.log(data)
+                // UI.SuccessMessage("get coords")
+                resolve(data)
+
+            },
+            error: (err)=>{
+                console.log(err)
+                UI.ErrorMessage(err)
+                reject(err)
+
+            }
+        });
+    })
+}
+
+
+function uploadFile(data,filename,pantryToken){
+    return new Promise((resolve,reject)=>{
+        $.ajax({
+            url : `https://getpantry.cloud/apiv1/pantry/${pantryToken}/basket/${filename}`,
+            type: "POST",
+            data : data,
+            async: false,
+            headers:{"Content-Type": "application/json"},
+            success: (response)=>{
+                console.log(response)
+                UI.SuccessMessage("upload coords",1000)
+                resolve(response)
+            },
+            error: (err)=>{
+                console.log(err)
+                UI.ErrorMessage(err)
+                reject(err)
+            }
+        });
+    })
+}
