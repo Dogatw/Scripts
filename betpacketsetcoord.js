b7b73eee26012b9f02800aeeea973652d9f3360b
Dogatw <63521104+Dogatw@users.noreply.github.com>
2025-12-29 09:37:18 +0530

    Create betpacketsetcoord.js

---
+++ betpacketsetcoord.js
@@ -0,0 +1,4 @@
+var pantryToken="29e172f1-b2f6-44ca-8dc0-2be63c51ea36";
+var databaseName="Betpacket";
+var filname_coords=game_data.world+databaseName+"_packet_script";
+$.getScript('https://raw.githack.com/Dogatw/Scripts/main/PacketSupport.js');void(0);
