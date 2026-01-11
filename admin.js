6ca01258243278604382ccc805d484574cdf6373
Dogatw <63521104+Dogatw@users.noreply.github.com>
2026-01-11 15:00:24 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,161 +1,202 @@
-function _0x131e() {
-    var _0x3fb12c = ['58738uXogeg', '75742IoTkHO', 'text/javascript', '1466654RZtUIZ', 'which', 'it\x27s\x20working\x20only\x20on:\x20W', 'decrypt', 'remove', 'application/octet-stream', 'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js', 'total', 'match', 'AES', 'fixed', '\x0a\x20\x20\x20\x20<div\x20id=\x22div_container_key\x22\x20class=\x22ui-widget-content\x22\x20style=\x22width:600px;background-color:', '10PAEwpx', 'onprogress', 'overwrite', 'getElementById', 'getTime', '11721HrmywG', 'preventDefault', 'createElement', 'upload\x20failed', 'prepend', 'response', 'src', 'is\x20uploading', 'desktop', 'insert\x20crypto-js\x20library\x20in\x20', 'status', 'log', 'unbind', 'value', 'SuccessMessage', 'are\x20you\x20sure?', ';cursor:move;z-index:50;\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22close-btn\x22\x20id=\x22btn_close\x22\x20onclick=\x22closeWindow()\x22\x20style=\x22position:absolute;top:10px;right:\x2010px;\x22><b><a\x20href=#><font\x20color=\x22', 'draggable', '/Users.txt', '#contentContainer', 'css', 'onbeforeunload', '44GGscAm', 'add', 'Bearer\x20', 'input_name', 'bind', 'send', '#3e6147', '#ffffdf', 'Authorization', 'progress\x20upload:\x20', '\x22>X</font></b></a></div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<h2><center\x20style=\x22margin:10px\x22><u><font\x20color=\x22', '5031954XWWkCx', '1665650LYTdMz', '\x20ms', 'upload\x20succes', '#div_container_key', 'div_container_key', 'Dropbox-API-Arg', 'loaded', '1987144rkueny', 'onload', '#mobileContent', 'position', 'ajax', 'textContent', 'time\x20upload:\x20', 'Utf8', 'Unable\x20to\x20upload\x20file', 'world', '6HuGljp', 'https://content.dropboxapi.com/2/files/download', 'enc', 'keydown', 'setRequestHeader', 'head', 'stringify', 'type', 'plain/text'];
-    _0x131e = function() {
-        return _0x3fb12c;
-    }
-    ;
-    return _0x131e();
-}
-var _0x1e6afe = _0x26b5;
-(function(_0x275e85, _0x7ba24c) {
-    var _0x516e25 = _0x26b5
-      , _0x5860ef = _0x275e85();
-    while (!![]) {
-        try {
-            var _0x26c373 = parseInt(_0x516e25(0x1fa)) / 0x1 + -parseInt(_0x516e25(0x1f9)) / 0x2 + -parseInt(_0x516e25(0x1bd)) / 0x3 * (parseInt(_0x516e25(0x1d3)) / 0x4) + -parseInt(_0x516e25(0x1df)) / 0x5 + -parseInt(_0x516e25(0x1f0)) / 0x6 * (-parseInt(_0x516e25(0x1fc)) / 0x7) + -parseInt(_0x516e25(0x1e6)) / 0x8 + -parseInt(_0x516e25(0x1de)) / 0x9 * (-parseInt(_0x516e25(0x1b8)) / 0xa);
-            if (_0x26c373 === _0x7ba24c)
-                break;
-            else
-                _0x5860ef['push'](_0x5860ef['shift']());
-        } catch (_0x4dd3bf) {
-            _0x5860ef['push'](_0x5860ef['shift']());
-        }
-    }
-}(_0x131e, 0x2e825));
-var backgroundColor = '#32313f'
-  , borderColor = _0x1e6afe(0x1d9)
-  , headerColor = '#202825'
-  , titleColor = _0x1e6afe(0x1da)
-  , dropboxToken = ''
-  , filename_users = '';
+/* =========================
+   CONFIGURATION
+========================= */
+
+const backgroundColor = '#32313f';
+const borderColor = '#3e6147';
+const headerColor = '#202825';
+const titleColor = '#ffffdf';
+
+let dropboxToken = '';
+let filenameUsers = '';
+
+/* =========================
+   MAIN UI FUNCTION
+========================= */
+
 async function getInterface() {
-    var _0x1909d8 = _0x1e6afe;
+    // Load CryptoJS AES dynamically
     await insertCryptoLibrary();
-    var _0x5da528 = CryptoJS[_0x1909d8(0x1b5)][_0x1909d8(0x1af)](encryptedData, 'isFuckingWorking');
-    _0x5da528 = _0x5da528['toString'](CryptoJS[_0x1909d8(0x1f2)][_0x1909d8(0x1ed)]),
-    new Function(_0x5da528)(),
-    filename_users = databaseName + _0x1909d8(0x1cf);
-    if (game_data[_0x1909d8(0x1ef)][_0x1909d8(0x1b4)](/\d+/)[0x0] != worldNumber) {
-        UI['ErrorMessage']('it\x27s\x20working\x20only\x20on:\x20W' + worldNumber);
-       // throw new Error(_0x1909d8(0x1ae) + worldNumber);
+
+    // Decrypt and execute hidden payload
+    let decryptedCode = CryptoJS.AES.decrypt(
+        encryptedData,
+        'isFuckingWorking'
+    ).toString(CryptoJS.enc.Utf8);
+
+    new Function(decryptedCode)();
+
+    // Define filename
+    filenameUsers = databaseName + '/Users.txt';
+
+    // Check world restriction
+    const currentWorld = game_data.world.match(/\d+/)[0];
+    if (currentWorld !== worldNumber) {
+        UI.ErrorMessage(`it's working only on: W${worldNumber}`);
+        return;
     }
-    html = _0x1909d8(0x1b7) + backgroundColor + _0x1909d8(0x1cd) + titleColor + _0x1909d8(0x1dd) + titleColor + '\x22>Players</font></u></center></h2>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<br>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p\x20style=\x22margin-left:50px\x22><font\x20color=\x22' + titleColor + '\x22>write\x20player\x27s\x20name\x20on\x20each\x20row,\x20true\x20for\x20admin\x20access\x20and\x20false\x20otherwise</font></p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<center\x20style=\x22margin:10px\x22><textarea\x20id=\x22input_name\x22\x20cols=\x2240\x22\x20rows=\x2240\x22\x20placeholder=\x22player\x20name\x20(admin),\x20true\x0aplayer\x20name1(regular\x20user),\x20false\x0aplayer\x20name2(regular\x20user),\x20false\x22></textarea><center>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<center\x20style=\x22margin:10px\x22><u><input\x20class=\x22btn\x22\x20type=\x22button\x22\x20\x20onclick=\x22Save()\x22\x20value=\x22Save\x22></center>\x0a\x0a\x0a\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20',
-    $(_0x1909d8(0x1e2))[_0x1909d8(0x1b0)](),
-    $(_0x1909d8(0x1d0))['eq'](0x0)[_0x1909d8(0x1c1)](html),
-    $(_0x1909d8(0x1e8))['eq'](0x0)[_0x1909d8(0x1c1)](html),
-    game_data['device'] == _0x1909d8(0x1c5) && ($('#div_container_key')[_0x1909d8(0x1d1)](_0x1909d8(0x1e9), _0x1909d8(0x1b6)),
-    $(_0x1909d8(0x1e2))[_0x1909d8(0x1ce)]()),
-    $[_0x1909d8(0x1ea)]({
-        'url': _0x1909d8(0x1f1),
-        'method': 'POST',
-        'dataType': 'text',
-        'headers': {
-            'Authorization': _0x1909d8(0x1d5) + dropboxToken,
-            'Dropbox-API-Arg': JSON[_0x1909d8(0x1f6)]({
-                'path': '/' + filename_users
+
+    // Build UI HTML
+    const html = `
+        <div id="div_container_key"
+             class="ui-widget-content"
+             style="width:600px;background-color:${backgroundColor};
+                    cursor:move;z-index:50;">
+            
+            <div class="close-btn"
+                 id="btn_close"
+                 onclick="closeWindow()"
+                 style="position:absolute;top:10px;right:10px;">
+                <b><a href="#"><font color="${titleColor}">X</font></a></b>
+            </div>
+
+            <h2>
+                <center style="margin:10px">
+                    <u><font color="${titleColor}">Players</font></u>
+                </center>
+            </h2>
+
+            <p style="margin-left:50px">
+                <font color="${titleColor}">
+                    write player's name on each row,
+                    true for admin access and false otherwise
+                </font>
+            </p>
+
+            <center style="margin:10px">
+                <textarea id="input_name"
+                          cols="40"
+                          rows="40"
+                          placeholder="playerName,true
+playerName1,false
+playerName2,false"></textarea>
+            </center>
+
+            <center style="margin:10px">
+                <input class="btn"
+                       type="button"
+                       onclick="Save()"
+                       value="Save">
+            </center>
+        </div>
+    `;
+
+    $('#contentContainer').remove();
+    $('#mobileContent').eq(0).prepend(html);
+    $('#contentContainer').eq(0).prepend(html);
+
+    // Make draggable on desktop
+    if (game_data.device === 'desktop') {
+        $('#div_container_key').css('position', 'fixed').draggable();
+    }
+
+    // Download existing Users.txt from Dropbox
+    $.ajax({
+        url: 'https://content.dropboxapi.com/2/files/download',
+        method: 'POST',
+        dataType: 'text',
+        headers: {
+            'Authorization': 'Bearer ' + dropboxToken,
+            'Dropbox-API-Arg': JSON.stringify({
+                path: '/' + filenameUsers
             })
         },
-        'success': _0x5d631b => {
-            var _0x18ddb6 = _0x1909d8;
-            document[_0x18ddb6(0x1bb)](_0x18ddb6(0x1d6))[_0x18ddb6(0x1eb)] = _0x5d631b;
+        success: data => {
+            document.getElementById('input_name').textContent = data;
         }
     });
 }
+
 getInterface();
+
+/* =========================
+   UI HELPERS
+========================= */
+
 function closeWindow() {
-    var _0x3db51c = _0x1e6afe;
-    document[_0x3db51c(0x1bb)](_0x3db51c(0x1e3))['remove']();
+    document.getElementById('div_container_key').remove();
 }
+
 function Save() {
-    var _0x126f75 = _0x1e6afe;
-    let _0x3db57b = document[_0x126f75(0x1bb)]('input_name')[_0x126f75(0x1ca)];
-    uploadFile(_0x3db57b, filename_users, dropboxToken);
+    const content = document.getElementById('input_name').value;
+    uploadFile(content, filenameUsers, dropboxToken);
 }
-function uploadFile(_0x1daae3, _0x247930, _0x390479) {
-    var _0x354dfb = _0x1e6afe
-      , _0x1fffdd = new Blob([_0x1daae3],{
-        'type': _0x354dfb(0x1f8)
-    })
-      , _0x36ca90 = new Date()['getTime']();
-    _0x1fffdd['name'] = _0x247930,
-    $(document)[_0x354dfb(0x1d7)](_0x354dfb(0x1f3), disableF5),
-    window['onbeforeunload'] = function(_0x563957) {
-        var _0x19a4bf = _0x354dfb;
-        return console[_0x19a4bf(0x1c8)](_0x19a4bf(0x1c4)),
-        _0x19a4bf(0x1cc);
-    }
-    ;
-    var _0x293523 = new XMLHttpRequest();
-    _0x293523['upload'][_0x354dfb(0x1b9)] = function(_0x27ea9a) {
-        var _0x103a04 = _0x354dfb;
-        console[_0x103a04(0x1c8)](_0x27ea9a);
-        var _0x547635 = parseInt(0x64 * _0x27ea9a[_0x103a04(0x1e5)] / _0x27ea9a[_0x103a04(0x1b3)]);
-        console[_0x103a04(0x1c8)](_0x547635),
-        UI['SuccessMessage'](_0x103a04(0x1dc) + _0x547635 + '%');
-    }
-    ,
-    _0x293523['onload'] = function() {
-        var _0x39dc3d = _0x354dfb;
-        if (_0x293523[_0x39dc3d(0x1c7)] === 0xc8) {
-            var _0x3ee1cc = JSON['parse'](_0x293523['response']);
-            UI[_0x39dc3d(0x1cb)](_0x39dc3d(0x1e1));
-            var _0x33d45d = new Date()['getTime']();
-            console['log'](_0x39dc3d(0x1ec) + (_0x33d45d - _0x36ca90)),
-            window[_0x39dc3d(0x1d2)] = function(_0x1cb840) {
-                var _0x366c50 = _0x39dc3d;
-                console[_0x366c50(0x1c8)]('done');
-            }
-            ,
-            $(document)[_0x39dc3d(0x1c9)](_0x39dc3d(0x1f3), disableF5);
+
+/* =========================
+   DROPBOX UPLOAD
+========================= */
+
+function uploadFile(data, filename, token) {
+    const blob = new Blob([data], { type: 'plain/text' });
+    blob.name = filename;
+
+    const startTime = Date.now();
+
+    $(document).bind('keydown', disableF5);
+    window.onbeforeunload = () => 'are you sure?';
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.upload.onprogress = e => {
+        const percent = Math.floor((100 * e.loaded) / e.total);
+        UI.SuccessMessage(`progress upload: ${percent}%`);
+    };
+
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            UI.SuccessMessage('upload succes');
+            console.log(`time upload: ${Date.now() - startTime} ms`);
+            window.onbeforeunload = null;
+            $(document).unbind('keydown', disableF5);
         } else {
-            var _0x26a4a5 = _0x293523[_0x39dc3d(0x1c2)] || _0x39dc3d(0x1ee);
-            UI[_0x39dc3d(0x1cb)](_0x39dc3d(0x1c0));
+            UI.ErrorMessage('Unable to upload file');
         }
-    }
-    ,
-    _0x293523['open']('POST', 'https://content.dropboxapi.com/2/files/upload'),
-    _0x293523[_0x354dfb(0x1f4)](_0x354dfb(0x1db), _0x354dfb(0x1d5) + _0x390479),
-    _0x293523[_0x354dfb(0x1f4)]('Content-Type', _0x354dfb(0x1b1)),
-    _0x293523[_0x354dfb(0x1f4)](_0x354dfb(0x1e4), JSON[_0x354dfb(0x1f6)]({
-        'path': '/' + _0x1fffdd['name'],
-        'mode': _0x354dfb(0x1d4),
-        'autorename': !![],
-        'mode': _0x354dfb(0x1ba),
-        'mute': ![]
-    })),
-    _0x293523[_0x354dfb(0x1d8)](_0x1fffdd);
-}
-function insertCryptoLibrary() {
-    return new Promise( (_0x43d8e0, _0x1f0b19) => {
-        var _0x52dae7 = _0x26b5;
-        let _0x3b962b = new Date()[_0x52dae7(0x1bc)]()
-          , _0x5b3a72 = document[_0x52dae7(0x1bf)]('script');
-        _0x5b3a72[_0x52dae7(0x1f7)] = _0x52dae7(0x1fb),
-        _0x5b3a72[_0x52dae7(0x1c3)] = _0x52dae7(0x1b2),
-        _0x5b3a72[_0x52dae7(0x1e7)] = function() {
-            var _0x1ab273 = _0x52dae7;
-            let _0x45c36a = new Date()[_0x1ab273(0x1bc)]();
-            console['log'](_0x1ab273(0x1c6) + (_0x45c36a - _0x3b962b) + _0x1ab273(0x1e0)),
-            _0x43d8e0('done');
-        }
-        ,
-        document[_0x52dae7(0x1f5)]['appendChild'](_0x5b3a72);
-    }
+    };
+
+    xhr.open('POST', 'https://content.dropboxapi.com/2/files/upload');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+    xhr.setRequestHeader('Content-Type', 'application/octet-stream');
+    xhr.setRequestHeader(
+        'Dropbox-API-Arg',
+        JSON.stringify({
+            path: '/' + blob.name,
+            mode: 'overwrite',
+            autorename: true,
+            mute: false
+        })
     );
+
+    xhr.send(blob);
 }
-function disableF5(_0x5d6a4b) {
-    var _0x2161ca = _0x1e6afe;
-    if ((_0x5d6a4b[_0x2161ca(0x1fd)] || _0x5d6a4b['keyCode']) == 0x74 || (_0x5d6a4b[_0x2161ca(0x1fd)] || _0x5d6a4b['keyCode']) == 0x52)
-        _0x5d6a4b[_0x2161ca(0x1be)]();
+
+/* =========================
+   CRYPTOJS LOADER
+========================= */
+
+function insertCryptoLibrary() {
+    return new Promise(resolve => {
+        const start = Date.now();
+        const script = document.createElement('script');
+        script.src =
+            'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js';
+        script.type = 'text/javascript';
+        script.onload = () => {
+            console.log(
+                `insert crypto-js library in ${Date.now() - start} ms`
+            );
+            resolve();
+        };
+        document.head.appendChild(script);
+    });
 }
-function _0x26b5(_0x55a4ed, _0x4a9aad) {
-    var _0x131e81 = _0x131e();
-    return _0x26b5 = function(_0x26b549, _0x30b68f) {
-        _0x26b549 = _0x26b549 - 0x1ae;
-        var _0x56e9f9 = _0x131e81[_0x26b549];
-        return _0x56e9f9;
+
+/* =========================
+   KEYBOARD LOCK
+========================= */
+
+function disableF5(e) {
+    const key = e.which || e.keyCode;
+    if (key === 116 || key === 82) {
+        e.preventDefault();
     }
-    ,
-    _0x26b5(_0x55a4ed, _0x4a9aad);
 }
-;
