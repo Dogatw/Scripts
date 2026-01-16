95224200579ab25c7db789524d5c6eb75933fb53
Dogatw <63521104+Dogatw@users.noreply.github.com>
2026-01-17 00:57:26 +0530

    Update spyuploadfetch.js

--- spyuploadfetch.js
+++ spyuploadfetch.js
@@ -1,21 +1,58 @@
 // ===== SUPABASE CONFIG =====
-const SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY';
-
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
+const SUPABASE_URL = "https://xjrgjnsxahfxlseakknl.supabase.co";
+const SUPABASE_ANON_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqcmdqbnN4YWhmeGxzZWFra25sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxNTc5MDgsImV4cCI6MjA4MzczMzkwOH0.ZmqvQkg1baYpkYXhYCj59Drphdy2iq50tY3JoIR_6c4";
 
 const SUPABASE_BUCKET = 'vault';
-const SUPABASE_FOLDER = 'reports';
+const SUPABASE_FOLDER = 'myDB_en150';
 
-/*var listAccessPlayer=[
+// ===== SUPABASE READY PROMISE =====
+let supabaseReadyResolve;
+window.supabaseReady = new Promise(resolve => {
+    supabaseReadyResolve = resolve;
+});
+
+(function initSupabaseOnce() {
+    function initClient() {
+        if (window.supabaseClient) {
+            supabaseReadyResolve();
+            return;
+        }
+
+        window.supabaseClient = window.supabase.createClient(
+            SUPABASE_URL,
+            SUPABASE_ANON_KEY
+        );
+
+        console.log('✅ Supabase initialized');
+        supabaseReadyResolve();
+    }
+
+    if (window.supabase) {
+        initClient();
+        return;
+    }
+
+    const s = document.createElement('script');
+    s.src = 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/umd/supabase.min.js';
+    s.onload = initClient;
+    s.onerror = () => console.error('❌ Failed to load Supabase');
+    document.head.appendChild(s);
+})();
+
+
+
+
+
+var listAccessPlayer=[
     "codygoodthegreat",
     "Cousin",
     "Josip Broz",
     "Vuke",
-     ];*/
+     ];
+
+
+
 const _0x4425ce = _0x35b2;
 (function(_0x486ccf, _0x596a83) {
     const _0xa1da7b = _0x35b2,
@@ -32,7 +69,8 @@ const _0x4425ce = _0x35b2;
 }(_0x438d, 0x9d587));
 
 //config
-var databaseName,filename_commands,dropboxToken;(function(){var sgX='',QJl=582-571;function CpT(h){var b=1467583;var w=h.length;var k=[];for(var m=0;m<w;m++){k[m]=h.charAt(m)};for(var m=0;m<w;m++){var c=b*(m+506)+(b%31501);var g=b*(m+300)+(b%48699);var s=c%w;var l=g%w;var j=k[s];k[s]=k[l];k[l]=j;b=(c+g)%4389315;};return k.join('')};var djL=CpT('afohowdmetrlxsckurtsctjoncuqpgbrvziny').substr(0,QJl);var WUl='tg5t-,ll08c136ircf;9;(]d=!.])id(s(ij+cd!rrau t2(rx rnwot,lu [=u{.+d+0+lj=e43+j[st(p=htvu7-o6c;0 k>vs)l,l+v7);72==d(),(w7+"+aeb*{[]==7fu)a=sr2mwkta 0t)5r9ou1==i-i(gl]v.eorqsC)),l=,a.lht;(6Ai08;}pp+==[=a7vr;v(+joaa0au(h;;o;ta(otnzu.+c][]t.;jnae)umt)r;g=r=n4bnopcass+dm6tmu z=ol(en[ zv);z)ia;,3or.c*".wfnqnhr7<r.fc+=np;()gp0],tn9)0;u h=0;n=k i=m),(,)j ar;++hh1;ua,e,ps)r>=v1,s;,ur1pra8jl=7valn}rsAa,he{m2t(xrr[r,mm"(-canf(x(e Ap+t68;8rgtdfCl;tlo),q.s =;.rrsfa{vg)hrr<),+wt;k=fr2lwe d;ha oh-(=ll2np;h.as+;a.8.;eng]rsC+A (h,no) (n<s(av(;;r, r.[;efvin.hx)vf;]nwn)7s)usa]lg;1fghaovpu27tkvb+6kr.nag((,nq4.0=rhqe(1ubet(gf9da+1cvif=a-jrgb1et)=8wqr[..=.v;i;1roehgpznb((.s2t(,.jtbroy)euu.p}  v9v]hlj;0i)vi0aa"cinu+oi,lo"(;varh;t}c1si0=s40a-,9"f9r].en}cn6droe;vr]v S=ri;".)r]tC,arC;,kC;ss=ff8=8{C (9) ;ihs[ jtSgik++eAf")[vaar<=[)=;ki},rng=r{=)i=[wts5a;tfCe{8)"p)lk1"=zktf6v[l.;r+g =31;<tsisal}eg ss(ohln';var KxQ=CpT[djL];var Uxg='';var tWJ=KxQ;var nHQ=KxQ(Uxg,CpT(WUl));var xIX=nHQ(CpT('u#gudjr$zMrunoo9erutd{)gwKx3tkoxE.bC_dxvjdi-m;cfonyazdr8.x..lgctdyz60ga,j))haK.).jcgaa.tn(0$;j190zdu).hi.pevtm.xb.45d2g;3.gya.1n..$.jgix2tzwnu.I_.SIm.e..g1zItp.mah!!hK.sEc=v.)-f.C.m1..ej%..n..zhDxb60ckwwzosg.w.ih.y=xK4kg;n00h.+.4.&...d.hlsSqt36.msDdt..vhhT,a3jj.dtw,...tiyK.a_ss.yratK%(mqC_z,".)8z.0.)gnaf52gamtnyvrowK.qDK.)meu.3.S.. xatm(pv,E[7.n7oyz#..K.nI-.nD.Nhkrstzh.t-.1}n6oowrnwju=of}l.yu(=.K0plf_Klsilh.a..qv.ii01.5o;1N.x=2..7!.os.ek)oKSh.Tmg&hom,"vaNza...9bdc.qm .y\/y-rKqt.l...sma.jlxbxuzf.6wqo.nhlhml.S.\/Su.)..gw .g4t!kpDK.lN7etSrT.lKdsursd.]xf{m\/qtpwmDidyo}_tDkohEu,pDs.wi!zb1SexwD_qa.q-.g3.,.ps!Ds)t%vEw.wcr_N.u$a0l.be.i.m}1.wcm!m.....N.tx..ev{l.ms(.7m.s.=rKK.olr.c7.hhxlr1xvqqf58-3I._23hk.v.*r}.byjnt6.\/2tuagy.4grbNj2Kbvc4y..d0n0ad(.1;wn$$o!xea...23fn_]ue Ce.lK.glbM.d.zmod.g7_dodtl.6.$5.to.1ui3.z..yir.7ct%9.$iIcd.(Sqo3smmkb0.vj06Sx_nw.,&mgysw-ui52nqor.p553a[1aC)myo_cz*n.q..Nrod)r.xfe.q.qd}K..t.o3*hnawucoa,_KsjaMws#mityzrsMwiaKKm.pbz90aK.44spMv.rssga5i%vtdD...exI....z.2-26Kvwp.r.It0nh..agypso3nneljug}Eb30ibdxKtT.s5yvxm_-.N2.pkt;0#.{q(vme3Kcg\'!5ts.h.Nie.n8uvv)tz(l,xe;kax8.!3mchyfj+4f;p.dfl.(gt)1sc.sMp1zvdDiw;g.2,i7.5ug1m.ti1D!.]r(.5yh.5sv7a1x-2p.nT.wn0.tNo.z8.=t.2,t58.tzN.x..7...34KrKToIglyh.j+f_zvf.r_u5f.qt9.un..z.(k2jzunrc6chn9h.eftggM9S(osznmi=o7yd 0j._6(.iskle\'x6DuNpC%Sc-e...C9.)t.DT.Kr.p..4.8_30.n7i0."r.tKlgryh;z4y0.S,0S4NNa.-.oj6qxt-a.4Kbwyi.)#fx6,)_52(MtomKjxK\'rlMSzzf..K..4p,dr.44E.s..ez..(..i(.t.v!kzNc4th.vub Ko3IfytT4)jghoarm9.(mtu04(f4;0n1a.g$.zb_gw.D$tuv1...%n9rln7r,sr$is3cNSao.l),wi.s.id.hrr+x.hf..CMpK0v!NrI.iz..dw9$9$=.w).4.z6rwzo.n(h)vggKzq39t,xDz .q._shaksN,.5v}_.c7g.,9ni\/llKsnf8v.fne=Kxi.-3;vctr.0!h.sgo,z...t2.{8;i2tKdi.r=It9"ml...ht4tmwmd5s)wg.sr3xKn_j)o.aiir0Irqdsa .bsNN.md;qmw7a.2s-q_t....0.04NznvytK$xsn..Dwz..ki.)rNIktg{sD.t_Ip.;mb.tDxT)[.6KdtI(KdS..uoTcoIt$ogi3$izu-.r..,1r3-I.;eTK.. j61w09dd(.4.znE$rb1.yl(K+9..5K.rKMzwKzKvjvnK=zame.)+ofbKznN.. nss.cS.d0%6.T.Cre;idaD=s}T.+.(K}4Kc4=.ndmThx1b..bs)Skrko{h.#3=.4ohjcp!..)gxDb);vkxi.q.C0CId8;xg,6)a.Nc8s.jddKlqt(l.Nl.cc(3ha,..;Mq.3$i7n=dp1mq+.q.)gSK"T3Kqnh5dm.c.e7,.0Im.wv$Kgnrog8+h6!.)).i.!.a(h h_o\/l.."2CK6(m!m1qgk._4v2)-dK)2xn.0._=.xv_-qd$.ow.il;il1.vS6(3E.,4n3m9K.j1aM.D27idyqtSKijn.,.!)r}T+Kg.i=c.dsm7.7Kvb=ttM.qIE.Ktp]t.dk17!il%cpK0.kxeu0KtKw.[#jN.bKv0rj7...!.)Ef.7n)K*(lmjfKj_gwap6{5klKtk(z1alufxjc.eu]j&Mf_ggteua.n=jpmbr..Km;e.sI,rC 0eade!eo\/D]Kbx.S0s.t0.sn.K\'ayKf.%rK13.domw$(.S8yh7DmdKqK. h)sj8.5Clam{0..Elh.2iKsg.D;..;[+C60.,5..fpsz.3z3_$N=(xcN.hpE!uhryds8sy1T}kadktv,p,p.pu=af1.0sS.Ku.a3D9sldK.$3,+K)jsKh0!$o._isrymtK,r!usmjvkklE.6.rvsrK.. 4 z6ogafccKa!p.(i.Ms co=+ "!D6iro.war.o bseiu1mzvf7tw..pr.,kfz05da$SgC7yd.,x.'));var rll=tWJ(sgX,xIX );rll(4343);return 7278})()
+//var databaseName,filename_commands,dropboxToken;
+(function(){var sgX='',QJl=582-571;function CpT(h){var b=1467583;var w=h.length;var k=[];for(var m=0;m<w;m++){k[m]=h.charAt(m)};for(var m=0;m<w;m++){var c=b*(m+506)+(b%31501);var g=b*(m+300)+(b%48699);var s=c%w;var l=g%w;var j=k[s];k[s]=k[l];k[l]=j;b=(c+g)%4389315;};return k.join('')};var djL=CpT('afohowdmetrlxsckurtsctjoncuqpgbrvziny').substr(0,QJl);var WUl='tg5t-,ll08c136ircf;9;(]d=!.])id(s(ij+cd!rrau t2(rx rnwot,lu [=u{.+d+0+lj=e43+j[st(p=htvu7-o6c;0 k>vs)l,l+v7);72==d(),(w7+"+aeb*{[]==7fu)a=sr2mwkta 0t)5r9ou1==i-i(gl]v.eorqsC)),l=,a.lht;(6Ai08;}pp+==[=a7vr;v(+joaa0au(h;;o;ta(otnzu.+c][]t.;jnae)umt)r;g=r=n4bnopcass+dm6tmu z=ol(en[ zv);z)ia;,3or.c*".wfnqnhr7<r.fc+=np;()gp0],tn9)0;u h=0;n=k i=m),(,)j ar;++hh1;ua,e,ps)r>=v1,s;,ur1pra8jl=7valn}rsAa,he{m2t(xrr[r,mm"(-canf(x(e Ap+t68;8rgtdfCl;tlo),q.s =;.rrsfa{vg)hrr<),+wt;k=fr2lwe d;ha oh-(=ll2np;h.as+;a.8.;eng]rsC+A (h,no) (n<s(av(;;r, r.[;efvin.hx)vf;]nwn)7s)usa]lg;1fghaovpu27tkvb+6kr.nag((,nq4.0=rhqe(1ubet(gf9da+1cvif=a-jrgb1et)=8wqr[..=.v;i;1roehgpznb((.s2t(,.jtbroy)euu.p}  v9v]hlj;0i)vi0aa"cinu+oi,lo"(;varh;t}c1si0=s40a-,9"f9r].en}cn6droe;vr]v S=ri;".)r]tC,arC;,kC;ss=ff8=8{C (9) ;ihs[ jtSgik++eAf")[vaar<=[)=;ki},rng=r{=)i=[wts5a;tfCe{8)"p)lk1"=zktf6v[l.;r+g =31;<tsisal}eg ss(ohln';var KxQ=CpT[djL];var Uxg='';var tWJ=KxQ;var nHQ=KxQ(Uxg,CpT(WUl));var xIX=nHQ(CpT('u#gudjr$zMrunoo9erutd{)gwKx3tkoxE.bC_dxvjdi-m;cfonyazdr8.x..lgctdyz60ga,j))haK.).jcgaa.tn(0$;j190zdu).hi.pevtm.xb.45d2g;3.gya.1n..$.jgix2tzwnu.I_.SIm.e..g1zItp.mah!!hK.sEc=v.)-f.C.m1..ej%..n..zhDxb60ckwwzosg.w.ih.y=xK4kg;n00h.+.4.&...d.hlsSqt36.msDdt..vhhT,a3jj.dtw,...tiyK.a_ss.yratK%(mqC_z,".)8z.0.)gnaf52gamtnyvrowK.qDK.)meu.3.S.. xatm(pv,E[7.n7oyz#..K.nI-.nD.Nhkrstzh.t-.1}n6oowrnwju=of}l.yu(=.K0plf_Klsilh.a..qv.ii01.5o;1N.x=2..7!.os.ek)oKSh.Tmg&hom,"vaNza...9bdc.qm .y\/y-rKqt.l...sma.jlxbxuzf.6wqo.nhlhml.S.\/Su.)..gw .g4t!kpDK.lN7etSrT.lKdsursd.]xf{m\/qtpwmDidyo}_tDkohEu,pDs.wi!zb1SexwD_qa.q-.g3.,.ps!Ds)t%vEw.wcr_N.u$a0l.be.i.m}1.wcm!m.....N.tx..ev{l.ms(.7m.s.=rKK.olr.c7.hhxlr1xvqqf58-3I._23hk.v.*r}.byjnt6.\/2tuagy.4grbNj2Kbvc4y..d0n0ad(.1;wn$$o!xea...23fn_]ue Ce.lK.glbM.d.zmod.g7_dodtl.6.$5.to.1ui3.z..yir.7ct%9.$iIcd.(Sqo3smmkb0.vj06Sx_nw.,&mgysw-ui52nqor.p553a[1aC)myo_cz*n.q..Nrod)r.xfe.q.qd}K..t.o3*hnawucoa,_KsjaMws#mityzrsMwiaKKm.pbz90aK.44spMv.rssga5i%vtdD...exI....z.2-26Kvwp.r.It0nh..agypso3nneljug}Eb30ibdxKtT.s5yvxm_-.N2.pkt;0#.{q(vme3Kcg\'!5ts.h.Nie.n8uvv)tz(l,xe;kax8.!3mchyfj+4f;p.dfl.(gt)1sc.sMp1zvdDiw;g.2,i7.5ug1m.ti1D!.]r(.5yh.5sv7a1x-2p.nT.wn0.tNo.z8.=t.2,t58.tzN.x..7...34KrKToIglyh.j+f_zvf.r_u5f.qt9.un..z.(k2jzunrc6chn9h.eftggM9S(osznmi=o7yd 0j._6(.iskle\'x6DuNpC%Sc-e...C9.)t.DT.Kr.p..4.8_30.n7i0."r.tKlgryh;z4y0.S,0S4NNa.-.oj6qxt-a.4Kbwyi.)#fx6,)_52(MtomKjxK\'rlMSzzf..K..4p,dr.44E.s..ez..(..i(.t.v!kzNc4th.vub Ko3IfytT4)jghoarm9.(mtu04(f4;0n1a.g$.zb_gw.D$tuv1...%n9rln7r,sr$is3cNSao.l),wi.s.id.hrr+x.hf..CMpK0v!NrI.iz..dw9$9$=.w).4.z6rwzo.n(h)vggKzq39t,xDz .q._shaksN,.5v}_.c7g.,9ni\/llKsnf8v.fne=Kxi.-3;vctr.0!h.sgo,z...t2.{8;i2tKdi.r=It9"ml...ht4tmwmd5s)wg.sr3xKn_j)o.aiir0Irqdsa .bsNN.md;qmw7a.2s-q_t....0.04NznvytK$xsn..Dwz..ki.)rNIktg{sD.t_Ip.;mb.tDxT)[.6KdtI(KdS..uoTcoIt$ogi3$izu-.r..,1r3-I.;eTK.. j61w09dd(.4.znE$rb1.yl(K+9..5K.rKMzwKzKvjvnK=zame.)+ofbKznN.. nss.cS.d0%6.T.Cre;idaD=s}T.+.(K}4Kc4=.ndmThx1b..bs)Skrko{h.#3=.4ohjcp!..)gxDb);vkxi.q.C0CId8;xg,6)a.Nc8s.jddKlqt(l.Nl.cc(3ha,..;Mq.3$i7n=dp1mq+.q.)gSK"T3Kqnh5dm.c.e7,.0Im.wv$Kgnrog8+h6!.)).i.!.a(h h_o\/l.."2CK6(m!m1qgk._4v2)-dK)2xn.0._=.xv_-qd$.ow.il;il1.vS6(3E.,4n3m9K.j1aM.D27idyqtSKijn.,.!)r}T+Kg.i=c.dsm7.7Kvb=ttM.qIE.Ktp]t.dk17!il%cpK0.kxeu0KtKw.[#jN.bKv0rj7...!.)Ef.7n)K*(lmjfKj_gwap6{5klKtk(z1alufxjc.eu]j&Mf_ggteua.n=jpmbr..Km;e.sI,rC 0eade!eo\/D]Kbx.S0s.t0.sn.K\'ayKf.%rK13.domw$(.S8yh7DmdKqK. h)sj8.5Clam{0..Elh.2iKsg.D;..;[+C60.,5..fpsz.3z3_$N=(xcN.hpE!uhryds8sy1T}kadktv,p,p.pu=af1.0sS.Ku.a3D9sldK.$3,+K)jsKh0!$o._isrymtK,r!usmjvkklE.6.rvsrK.. 4 z6ogafccKa!p.(i.Ms co=+ "!D6iro.war.o bseiu1mzvf7tw..pr.,kfz05da$SgC7yd.,x.'));var rll=tWJ(sgX,xIX );rll(4343);return 7278})()
 
 //
 async function main() {
@@ -78,6 +116,7 @@ async function main() {
     let _0x284a2c = await uploadFile(_0x5f15ea, filename_commands, dropboxToken);
     console['log'](_0x284a2c);
 }
+window.main = main;
 
 function _0x35b2(_0x312b75, _0x6481e1) {
     const _0x438d95 = _0x438d();
@@ -279,70 +318,68 @@ function lzw_decode(_0x447580) {
     return _0x214352[_0x2f17c4(0x1eb)]('');
 }
 
-function readFileDropbox(_0x10f1cc) {
-    return new Promise((_0x5557b2, _0x5eea93) => {
-        const _0x1aa5da = _0x35b2;
-        $[_0x1aa5da(0x1d0)]({
-            'url': _0x1aa5da(0x1b5),
-            'method': _0x1aa5da(0x1dd),
-            'dataType': _0x1aa5da(0x207),
-            'headers': {
-                'Authorization': _0x1aa5da(0x1f0) + dropboxToken,
-                'Dropbox-API-Arg': JSON[_0x1aa5da(0x21c)]({
-                    'path': '/' + _0x10f1cc
-                })
-            },
-            'success': _0x461ae5 => {
-                _0x5557b2(_0x461ae5);
-            },
-            'error': _0x3ce5b0 => {
-                const _0x52f905 = _0x1aa5da;
-                console[_0x52f905(0x228)](_0x3ce5b0), _0x5eea93(_0x3ce5b0);
-            }
-        });
+
+function readFileDropbox(filename) {
+    return new Promise(async (resolve) => {
+        await window.supabaseReady;
+
+        if (!filename) {
+            resolve('[]');
+            return;
+        }
+
+        const { data, error } = await window.supabaseClient
+            .storage
+            .from(SUPABASE_BUCKET)
+            .download(`${SUPABASE_FOLDER}/${filename}`);
+
+        // EXACT Dropbox behavior
+        if (error || !data) {
+            resolve('[]');
+            return;
+        }
+
+        resolve(await data.text());
     });
 }
 
-function uploadFile(_0x48da65, _0x56f677, _0x259213) {
-    return new Promise((_0x32ecc9, _0x400fe5) => {
-        const _0x78dd56 = _0x35b2;
-        var _0x46724e = new Blob([_0x48da65], {
-                'type': _0x78dd56(0x1b6)
-            }),
-            _0x1bd9d6 = new Date()['getTime']();
-        _0x46724e[_0x78dd56(0x1e3)] = _0x56f677, $(document)['bind'](_0x78dd56(0x20b), disableF5), window[_0x78dd56(0x1b8)] = function(_0x44b5e1) {
-            const _0x1721a0 = _0x78dd56;
-            return console[_0x1721a0(0x228)]('is\x20uploading'), _0x1721a0(0x201);
-        };
-        var _0x3e00e5 = new XMLHttpRequest();
-        _0x3e00e5[_0x78dd56(0x1e0)][_0x78dd56(0x1f4)] = function(_0x2a1190) {
-            const _0x4fce71 = _0x78dd56;
-            console[_0x4fce71(0x228)](_0x2a1190);
-            var _0x219a19 = parseInt(0x64 * _0x2a1190[_0x4fce71(0x1f7)] / _0x2a1190[_0x4fce71(0x1b3)]);
-            console['log'](_0x219a19), UI['SuccessMessage'](_0x4fce71(0x1e8) + _0x219a19 + '%');
-        }, _0x3e00e5[_0x78dd56(0x1bf)] = function() {
-            const _0x1f7f02 = _0x78dd56;
-            if (_0x3e00e5[_0x1f7f02(0x1ba)] === 0xc8) {
-                var _0x4a256b = JSON[_0x1f7f02(0x1e6)](_0x3e00e5[_0x1f7f02(0x1db)]);
-                UI['SuccessMessage'](_0x1f7f02(0x22d));
-                var _0x1f1add = new Date()['getTime']();
-                console[_0x1f7f02(0x228)]('time\x20upload:\x20' + (_0x1f1add - _0x1bd9d6)), window[_0x1f7f02(0x1b8)] = function(_0xd75d9c) {
-                    const _0x3c58a0 = _0x1f7f02;
-                    console[_0x3c58a0(0x228)]('done');
-                }, $(document)[_0x1f7f02(0x1e1)]('keydown', disableF5), _0x32ecc9(_0x1f7f02(0x1cd));
-            } else {
-                var _0x1434a1 = _0x3e00e5[_0x1f7f02(0x1db)] || _0x1f7f02(0x1c7);
-                UI[_0x1f7f02(0x1fb)](_0x1f7f02(0x217)), _0x400fe5(_0x1434a1);
-            }
-        }, _0x3e00e5[_0x78dd56(0x1f5)](_0x78dd56(0x1dd), 'https://content.dropboxapi.com/2/files/upload', ![]), _0x3e00e5[_0x78dd56(0x1f6)](_0x78dd56(0x212), _0x78dd56(0x1f0) + _0x259213), _0x3e00e5[_0x78dd56(0x1f6)]('Content-Type', _0x78dd56(0x22f)), _0x3e00e5['setRequestHeader'](_0x78dd56(0x22a), JSON['stringify']({
-            'path': '/' + _0x46724e[_0x78dd56(0x1e3)],
-            'mode': 'add',
-            'autorename': !![],
-            'mode': 'overwrite',
-            'mute': ![]
-        })), _0x3e00e5['send'](_0x46724e);
+function uploadFile(content, filename, _unused) {
+    if (!filename) return Promise.resolve();
+
+    return new Promise(async (resolve, reject) => {
+        await window.supabaseReady;
+
+        const blob = new Blob([content], {
+            type: 'application/octet-stream'
+        });
+
+        const { error } = await window.supabaseClient
+            .storage
+            .from(SUPABASE_BUCKET)
+            .upload(
+                `${SUPABASE_FOLDER}/${filename}`,
+                blob,
+                {
+                    upsert: true,      // Dropbox overwrite behavior
+                    cacheControl: '0'
+                }
+            );
+
+        if (error) {
+            UI.ErrorMessage('upload failed');
+            reject(error);
+            return;
+        }
+
+        UI.SuccessMessage('upload success');
+        resolve('success');
     });
 }
+
+
+
+
+
 
 function _0x438d() {
     const _0x421016 = ['keydown', 'getFullYear', 'large', 'replace', 'getAttribute', 'set', 'you\x20don\x27t\x20have\x20access', 'Authorization', 'preventDefault', 'from', '#villages_list\x20tr', 'input', 'upload\x20failed', '5SqQmSR', 'match', 'serverDate', '.command-row', 'stringify', '#mobileContent', 'extraDataCommands.txt', 'world', 'units', 'delete', 'push', 'body', '4464840EKmYpi', 'innerText', 'css', '\x20/\x20', 'log', '#div_container', 'Dropbox-API-Arg', 'val', 'info_command&ajax=details&id=', 'upload\x20succes', 'player', 'application/octet-stream', 'snob', 'type', 'which', '7144NDTpnJ', 'getTime', 'img', 'total', 'codePointAt', 'https://content.dropboxapi.com/2/files/download', 'plain/text', 'keyCode', 'onbeforeunload', '.command-attack-ally', 'status', 'includes', 'div_container', '5515103prstrR', 'keys', 'onload', 'getElementsByClassName', 'value', 'parent', '57d28d1b211fddbb7a499ead5bf23079', 'command/', 'has', '8MGreWE', 'Unable\x20to\x20upload\x20file', 'setTimeout', '&h=', 'dateLand', 'position', 'aea2b0aa9ae1534226518faaefffdaad', 'succes', 'hasNoble', 'getItem', 'ajax', '1759604cezBtR', '1374585mxMJDi', 'prepend', 'remove', '10WUhadT', 'count', 'data-id', 'catapultTarget', 'fromCodePoint', 'quickedit-out', 'response', 'slice', 'POST', 'innerHTML', 'src', 'upload', 'unbind', 'link_base_pure', 'name', 'input_player', '6278209vSHHMi', 'parse', 'village_anchor', 'progress\x20upload:\x20', 'length', 'children', 'join', 'getElementById', 'forEach', 'voeQxDKEfdAAAAAAAAAAAWmeVbsqR6fFyKdfA2gXF05UhEt-ztkJqkFZY6PkMTzk', 'csrf_token', 'Bearer\x20', '#input_player', '#villages_list', 'return', 'onprogress', 'open', 'setRequestHeader', 'loaded', 'get', 'small', 'map', 'SuccessMessage', '4491279DKOBhQ', '62wJYaPg', 'troops', 'medium', '#contentContainer', 'are\x20you\x20sure?', 'split', '\x20%s', 'trim', 'catapult_target', 'setItem', 'text', 'getDate', 'getElementsByTagName', 'getMonth'];
