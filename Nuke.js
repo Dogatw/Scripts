8fa756b2aa3e43b73ad2f754b0a66d968722322a
Dogatw <63521104+Dogatw@users.noreply.github.com>
2026-01-25 12:01:27 +0530

    Update Nuke.js

--- Nuke.js
+++ Nuke.js
@@ -1,36 +1,37 @@
 /*Approved link for script below
 https://forum.tribalwars.net/index.php?threads/rally-point-form-filling-nuke-script.287514/
 */
-var pantryToken = "29e172f1-b2f6-44ca-8dc0-2be63c51ea36"; 
-var databaseName = "NAM";
-var filname_coords = game_data.world + databaseName + "_coords_nuke_script";
-var forceEqualTroopCount = false,
-    distributeByPopulation = false,
-    forceRamSpeed = true,
-    troops = {
-        "spear": '0',
-        "sword": '0',
-        "axe": 'max',
-        "archer": '0',
-        "spy": '1',
-        "light": 'max',
-        "marcher": 'max',
-        "heavy": '0',
-        "ram": '250',
-        "catapult": 'max'
-    },
-    /*Allowed troop inputs: numbers, 'min', 'max' and 'min!'. 'min!' will force at least one of this unit */ populationLim = 0;
-var minTroops={
-    "spear": '0',
-    "sword": '0',
-    "axe": '1000',
-    "archer": '0',
-    "spy": '0',
-    "light": '500',
-    "marcher": '0',
-    "heavy": '0',
-    "ram": '0',
-    "catapult": '0'
+/* ================= TROOP CONFIG ================= */
+
+const forceEqualTroopCount = false;
+const distributeByPopulation = false;
+const forceRamSpeed = true;
+const populationLim = 0;
+
+const troops = {
+    spear: '0',
+    sword: '0',
+    axe: 'max',
+    archer: '0',
+    spy: '1',
+    light: 'max',
+    marcher: 'max',
+    heavy: '0',
+    ram: '250',
+    catapult: 'max'
+};
+
+const minTroops = {
+    spear: '0',
+    sword: '0',
+    axe: '1000',
+    archer: '0',
+    spy: '0',
+    light: '500',
+    marcher: '0',
+    heavy: '0',
+    ram: '0',
+    catapult: '0'
 };
 $.getScript('https://raw.githack.com/Dogatw/Scripts/main/TribalNukeScript.js');
 void(0)
