581e7e5122753e1d98e943f9a62fe75d10bec06f
Dogatw <63521104+Dogatw@users.noreply.github.com>
2026-01-25 12:16:00 +0530

    Update Nuke.js

--- Nuke.js
+++ Nuke.js
@@ -1,6 +1,14 @@
 /*Approved link for script below
 https://forum.tribalwars.net/index.php?threads/rally-point-form-filling-nuke-script.287514/
 */
+
+/* ================= CONFIG ================= */
+
+ const SUPABASE_URL = "https://xjrgjnsxahfxlseakknl.supabase.co";
+    const SUPABASE_ANON_KEY =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqcmdqbnN4YWhmeGxzZWFra25sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxNTc5MDgsImV4cCI6MjA4MzczMzkwOH0.ZmqvQkg1baYpkYXhYCj59Drphdy2iq50tY3JoIR_6c4";
+
+
 /* ================= TROOP CONFIG ================= */
 
 const forceEqualTroopCount = false;
